db04fdb47e980eb2921fe7c7d69e1ef25a6ba43b
Baser on Chain <60061293+baseronchain@users.noreply.github.com>
2025-12-07 11:56:04 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+const { ethers } = require('ethers');
+const fs = require('fs').promises;
+const readline = require('readline');
+
+const colors = {
+  reset: "\x1b[0m",
+  cyan: "\x1b[36m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  red: "\x1b[31m",
+  white: "\x1b[37m",
+  bold: "\x1b[1m",
+  magenta: "\x1b[35m",
+};
