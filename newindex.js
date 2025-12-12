a9b0890ef50d189f2465440365aa7ad32de7e3a3
Baser on Chain <60061293+baseronchain@users.noreply.github.com>
2025-12-12 16:57:20 +0700

    Create newindex.js

---
+++ newindex.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const { ethers } = require('ethers');
+const fs = require('fs').promises;
+const readline = require('readline');
+
+const colors = {
+  reset: "\x1b[0m",
+  cyan: "\x1b[36m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  red: "\x1b[31m",
+  white: "\x1b[37m",
+  bold: "\x1b[1m",
+  magenta: "\x1b[35m",
+};
+
+const logger = {
+  info: (msg) => console.log(`${colors.white}[✓] ${msg}${colors.reset}`),
+  warn: (msg) => console.log(`${colors.yellow}[⚠] ${msg}${colors.reset}`),
+  error: (msg) => console.log(`${colors.red}[✗] ${msg}${colors.reset}`),
+  success: (msg) => console.log(`${colors.green}[✅] ${msg}${colors.reset}`),
+  loading: (msg) => console.log(`${colors.cyan}[→] ${msg}${colors.reset}`),
+  banner: () => {
+    console.log(`${colors.cyan}${colors.bold}`);
+    console.log(`--------------------------------------`);
+    console.log(`    Baseronchain  `);
+    console.log(`--------------------------------------${colors.reset}`);
+  },
+};
+
+const USER_AGENTS = [
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
+];
+
+const PRIVY_APP_ID = "cmhjr62d90005ky0cnauy4zgm";
+const WALLETS_FILE = "wallets.json"
