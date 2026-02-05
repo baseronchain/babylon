b3ada1b4b4eb91248e8baa1cdf0383019b45abc1
Baser on Chain <60061293+baseronchain@users.noreply.github.com>
2026-02-05 21:36:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -26,3 +26,8 @@ const logger = {
     console.log(`--------------------------------------${colors.reset}`);
   },
 };
+const USER_AGENTS = [
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
+];
